9c5fd82a7001d0149eff9c51104dc95daa5cbb1b
Eko Nur Adi P <ekonuradip@gmail.com>
2024-07-23 14:15:00 +0700

    Halaman Utama Web Portofolio

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed= new Typed(".text", {
+    strings:["Front-End Developer", "Youtuber","Mobile Developer", "Web Developer", "Fullstack Developer", "Data Analis", "Data Scientist", "Flutter Developer"],
+    typeSpeed: 100, 
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
+var nameTyped= new Typed(".eko", {
+    strings:["Eko Nur Adi Prasetyo"],
+    typeSpeed: 200, 
+    backSpeed: 50,
+    backDelay: 2000,
+    loop:true
+});
